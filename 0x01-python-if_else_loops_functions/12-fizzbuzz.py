ff145226c91bb0e1ce340a37bfecddb564fb2c9e
karama gueye <karamagueye04@gmail.com>
2023-11-29 17:59:57 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,4 +1,11 @@
 #!/usr/bin/puython3
 def fizzbuzz():
     for i in range(1, 100)
-    if i % 3 == and i 
+    if i % 3 == and i % 5 != 0:
+        print("Fizz ", end='')
+    elif i % 5 == 0 and i % 3 != 0:
+        print("Buzz ", end='')
+    elif i % 5== 0 and i % 3 == 0:
+        print("FizzBuzz ", end='')
+    else:
+        print("{} ".format(i=, end='')
