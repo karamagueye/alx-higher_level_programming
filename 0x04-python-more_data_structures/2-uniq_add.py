814f3c725c346cd6ac40e0d30ef5cba9ef5a18b5
karama gueye <karamagueye04@gmail.com>
2023-12-06 14:42:07 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    uniq_add = set(my_list)
+    uniq_list = set (my_list)
     num = 0
-    for i in uniq_add:
+    for i in uniq_list:
         num += i
-        return (num)
+        return num
