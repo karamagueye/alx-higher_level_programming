55ffc108127fdb50c7e4179d1cdf999cbf674c44
karama gueye <karamagueye04@gmail.com>
2023-12-19 14:32:08 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
+"""Square module"""
 
+
+class Square:
+    """Defines a square."""
+
+
+    def __init__(self, size=0):
+        """constructure.
+
+
+        Args:
+            size: Length of a side of the square.
+
+
+           Raises:
+               TypeError: if size is not an interger
+               ValueError: if size is less than 0
+           """
+            if not isinstance(size, int):
+                raise TypeError('size must be an interger')
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            self.__size = size
