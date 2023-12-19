6ccc0b7a0ae9287a68846c6e9c45d0238e6c9e4b
karama gueye <karamagueye04@gmail.com>
2023-12-19 13:11:42 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class Square:
+    """Define a Square."""
+
+    def _int_(self, size):
+        """constructor.
+
+        size: lenght of a the square.
+
+        """
+        self._size = size
