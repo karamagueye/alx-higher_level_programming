f9f2d2d30fcc8bcf008441ea36c1de5275760c05
karama gueye <karamagueye04@gmail.com>
2024-01-09 07:03:18 -0800

    7-base_geometry.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,13 +1,31 @@
 #!/usr/bin/python3
-"""Define"""
+""" 0x0A. Python - Inheritance, task 7 """
+
+
 class BaseGeometry:
-    """ represent bvasre geometry"""
+    """Intentionally empty area() method.
+
+    """
     def area(self):
-        """Not yet implement"""
-        raise Exception("area() is not implemented")
+        """Unimplemented, only raises exception to notify user.
+
+        """
+        raise Exception('area() is not implemented')
+
     def integer_validator(self, name, value):
-        """value a parameter as an integer"""
+        """Input filtration for integers: checks if value is an int above 0.
+        No use of self, could be staticmethod.
+
+        Args:
+            name (str): name bound to object
+            value (any): value of object, expecting int
+
+        Exceptions:
+            TypeError: if `value` is not an int
+            ValueError: if `value` is less than or equal to 0
+
+        """
         if type(value) != int:
-            raise TypeError("{}must be an interger"format(name))
+            raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
-            raise ValueError("{} must be freater tfhan 0".format(name))
+            raise ValueError('{} must be greater than 0'.format(name))
