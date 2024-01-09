0385dd67c4be16551cfe7e293e085651249ee476
karama gueye <karamagueye04@gmail.com>
2024-01-08 16:01:32 -0800

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define"""
+def inherits_from(obj, a_class):
+    """Check
+    Args:
+    Return:
+    """
+    if issubclass(type(obj).a_class) and type(obj) != a_class:
+        return False
+    return False
